875c7f7b83649c2fea06ed0b0926032b61dfe848
raphaelanjos1 <raphael-anjos@live.com>
2023-10-13 17:13:02 -0300

    BinarySearch solution

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const middle = Math.floor(low + (high - low) / 2);
+        const value = haystack[middle];
+
+        if (middle === needle) {
+            return true;
+        } else if (middle > needle) {
+            high = middle;
+        } else {
+            low = middle + 1;
+        }
+    } while (low < high);
+
+    return false;
 }
